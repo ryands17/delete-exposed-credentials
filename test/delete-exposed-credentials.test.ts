7c60d96a9452611e7e2a8fd9ba3802a53a430406
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2021-08-10 17:19:02 +0530

    add tests and gh workflow

--- test/delete-exposed-credentials.test.ts
+++ test/delete-exposed-credentials.test.ts
@@ -1,13 +1,100 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as DeleteExposedCredentials from '../lib/delete-exposed-credentials-stack';
+import './helpers'
+import { Template } from '@aws-cdk/assertions'
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as DeleteExposedCredentials from '../lib/delete-exposed-credentials-stack'
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new DeleteExposedCredentials.DeleteExposedCredentialsStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
-});
+const LAMBDAS_GENERATED_BY_STACK = 1
+
+const createStack = () => {
+  const app = new cdk.App()
+  return new DeleteExposedCredentials.DeleteExposedCredentialsStack(
+    app,
+    'DeleteExposedCredentialsStack'
+  )
+}
+
+test('SNS Topic and Subscription are created', () => {
+  const assert = Template.fromStack(createStack())
+
+  assert.hasResource('AWS::SNS::Topic', {})
+
+  assert.hasResourceProperties('AWS::SNS::Subscription', {
+    Protocol: 'email',
+  })
+})
+
+test('Has the correct amount of Lambdas and structure', () => {
+  const assert = Template.fromStack(createStack())
+
+  assert.resourceCountIs(
+    'AWS::Lambda::Function',
+    3 + LAMBDAS_GENERATED_BY_STACK
+  )
+
+  assert.hasResourceProperties('AWS::Lambda::Function', {
+    Environment: {
+      Variables: {
+        NODE_OPTIONS: '--enable-source-maps',
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      },
+    },
+    Handler: 'index.handler',
+    MemorySize: 256,
+    ReservedConcurrentExecutions: 5,
+    Runtime: 'nodejs14.x',
+    Timeout: 5,
+  })
+})
+
+test('Has appropriate permissions to access CloudTrail and SNS', () => {
+  const stack = createStack()
+  const assert = Template.fromStack(stack)
+
+  assert.hasResourceProperties('AWS::IAM::Policy', {
+    PolicyDocument: {
+      Statement: [
+        {
+          Action: 'cloudtrail:LookupEvents',
+          Effect: 'Allow',
+          Resource: '*',
+        },
+      ],
+      Version: '2012-10-17',
+    },
+  })
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::IAM::Policy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 'sns:Publish',
+            Effect: 'Allow',
+            Resource: {},
+          },
+        ],
+        Version: '2012-10-17',
+      },
+    })
+  )
+})
+
+test('State Machine and Event to trigger it are created', () => {
+  const assert = Template.fromStack(createStack())
+
+  assert.hasResource('AWS::StepFunctions::StateMachine', {})
+
+  assert.hasResourceProperties('AWS::Events::Rule', {
+    EventPattern: {
+      source: ['aws.health'],
+      'detail-type': ['AWS Health Event'],
+      detail: {
+        service: ['RISK'],
+        eventTypeCategory: ['issue'],
+        eventTypeCode: ['AWS_RISK_CREDENTIALS_EXPOSED'],
+      },
+    },
+    State: 'ENABLED',
+  })
+})
--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    CDK_DEFAULT_REGION?: string
     AWS_REGION?: string
     SNS_ENDPOINT: string
     TOPIC_ARN: string
